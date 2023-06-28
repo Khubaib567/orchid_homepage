102c4fcf0a3a618b298349b4b1e9292298030b9e
Khubaib Ahmed <Khubaibahmed567@gmail.com>
2023-06-28 18:15:01 +0500

    remove_admin_page

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ import Hotel from "./pages/hotel/Hotel";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Checkout from "./pages/checkout/Checkout";
-import List from "./pages/list/List";
+// import List from "./pages/list/List";
 import { hotelColumns, roomColumns, userColumns } from "./datatablesource";
-import Admin from "./pages/admin/Admin";
+// import Admin from "./pages/admin/Admin";
 import AdminLogin from "./pages/admin_login/Login"
 // import NewHotel from "../../admin/src/pages/newHotel/NewHotel";
 // import NewRoom from "../../admin/src/pages/newRoom/NewRoom";
@@ -21,13 +21,13 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/hotels" element={<List/>}/>
+        {/* <Route path="/hotels" element={<List/>}/> */}
         <Route path="/hotels/:id" element={<Hotel/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/checkout" element={<Checkout />}/>
-        <Route path="/admin" element={<Admin />}/>
-        <Route path="/adminhotels" element={<List columns={hotelColumns}/>}/>
-        <Route path="/adminRooms" element={<List columns={roomColumns} />}/>
+        {/* <Route path="/admin" element={<Admin />}/> */}
+        {/* <Route path="/adminhotels" element={<List columns={hotelColumns}/>}/> */}
+        {/* <Route path="/adminRooms" element={<List columns={roomColumns} />}/> */}
         <Route path="/adminlogin" element={<AdminLogin />}/>
         <Route path="/register" element={<Register/>}/>
 
