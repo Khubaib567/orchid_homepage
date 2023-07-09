8d53b80b681647462d73b8dd6a955e76b8882c79
Khubaib Ahmed <Khubaibahmed567@gmail.com>
2023-07-09 13:19:29 +0500

    change_width

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -15,21 +15,21 @@ const Categories = () => {
       {/* </div> */}
       <div class="carousel w-full">
         <div id="slide1" class="carousel-item relative w-full">
-          <img src="https://i.ibb.co/YLvmhgn/room1.jpg" class="w-full" />
+          <img src="https://i.ibb.co/YLvmhgn/room1.jpg" class="w-10" />
           <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide3" class="btn btn-ghost text-black">❮</a>
             <a href="#slide2" class="btn btn-ghost text-black">❯</a>
           </div>
         </div>
         <div id="slide2" class="carousel-item relative w-full">
-          <img src="https://i.ibb.co/BPG7QKj/room2.jpg" class="w-full" />
+          <img src="https://i.ibb.co/BPG7QKj/room2.jpg" class="w-10" />
           <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide1" class="btn btn-ghost text-black">❮</a>
             <a href="#slide3" class="btn btn-ghost text-black">❯</a>
           </div>
         </div>
         <div id="slide3" class="carousel-item relative w-full">
-          <img src="https://i.ibb.co/hdKJstd/room3.jpg" class="w-full" />
+          <img src="https://i.ibb.co/hdKJstd/room3.jpg" class="w-10" />
           <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" class="btn btn-ghost text-black">❮</a>
             <a href="#slide1" class="btn btn-ghost text-black">❯</a>
