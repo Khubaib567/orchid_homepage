63191fb893b4149d69f0e15f0df98d50ca06d1ab
Khubaib Ahmed <Khubaibahmed567@gmail.com>
2023-07-09 13:17:22 +0500

    modify_slider_url

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -29,7 +29,7 @@ const Categories = () => {
           </div>
         </div>
         <div id="slide3" class="carousel-item relative w-full">
-          <img src="https://i.ibb.co/YLvmhgn/room3.jpg" class="w-full" />
+          <img src="https://i.ibb.co/hdKJstd/room3.jpg" class="w-full" />
           <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" class="btn btn-ghost text-black">❮</a>
             <a href="#slide1" class="btn btn-ghost text-black">❯</a>
