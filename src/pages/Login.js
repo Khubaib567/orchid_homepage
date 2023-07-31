e4b9dbe64cc07ba69b648af6b7e981af49d6644e
Khubaib567 <49817481+Khubaib567@users.noreply.github.com>
2023-07-31 16:37:32 +0500

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const validateAndRedirect = (user) => {
+  // Assuming you have a 'user' object with registration information
+  // You can replace this with your actual user validation logic
+  const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User not found!"));
+
+   const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+  if (isPasswordCorrect) {
+    // If user is registered, redirect to the root page
+    navigate("/")
+  } else {
+    // If user is not registered, display an error message or take appropriate action
+    alert('You are not registered. Please sign up to continue.');
+    // You can also redirect to a registration page instead of showing an alert
+     // navigate("/"register)
+  }
+};
 
 
 export default function Login(){
@@ -19,7 +42,7 @@ export default function Login(){
     <input type="text" placeholder="Password here" class="input input-bordered input-lg bg-white" />
   </label>
 </div>
-<div class="flex justify-center"><button class="btn btn-primary">Log In</button></div>
+<div class="flex justify-center"><button class="btn btn-primary" onClick={handleClick}>Log In</button></div>
 </div>
 
 
